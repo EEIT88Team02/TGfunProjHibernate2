017a5daae1c583eaccd0d547d743c4983e928c2a
student <student@192.168.127.128>
2016-10-11 22:58:00 +0800

    Initial project

---
+++ test/src/main/java/model/Interface/CommentInterface.java
@@ -0,0 +1,38 @@
+package model.Interface;
+
+import java.util.Date;
+import java.util.List;
+
+import model.CommentBean;
+
+public interface CommentInterface {
+
+	// select
+
+	/* 王荐進(頭) */
+	public abstract List<CommentBean> selectAll();
+
+	public abstract List<CommentBean> selectByMemberID(int memberID);
+
+	public abstract List<CommentBean> selectByCommentID(int commentID);
+
+	public abstract List<CommentBean> selectByCommentDate(Date commentDate);
+
+	public abstract List<CommentBean> selectByReply(String reply);
+	/* 王荐進(尾) */
+
+	
+	// insert
+
+	/* 王荐進(頭) */
+	public abstract boolean insert(CommentBean commentBean);
+	/* 王荐進(尾) */
+
+	
+	// update
+
+	/* 王荐進(頭) */
+	public abstract CommentBean update(int CommentID, int memberID, String commentContent, java.util.Date commentDate, String reply);
+	/* 王荐進(尾) */
+
+}
